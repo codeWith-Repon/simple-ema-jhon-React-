5a4888747e07eaa6cd8d61b638c25bef6dba5284
Md Repon <repon7253@gmail.com>
2024-09-10 15:41:24 +0600

    add some function(x)

--- src/components/Revew/Review.jsx
+++ src/components/Revew/Review.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getDatabaseCart } from "../../assets/utilities/databaseManager";
 
 const Review = () => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const savedCart = getDatabaseCart();
+    const productKeys = Object.keys(savedCart)
+    const coutns = productKeys.map((key)=> savedCart[key])
+    console.log(coutns);
+  }, []);
+
   return (
     <div>
       <h1>this is review page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Review
+export default Review;
