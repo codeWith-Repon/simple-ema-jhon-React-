cb05aaf808354d3b3adabcbe7189569c5aa3bd26
Md Repon <repon7253@gmail.com>
2024-10-05 11:11:45 +0600

    configure firebase

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,142 @@
-import React from 'react'
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
+import firebaseConfig from "./firebase.config";
 
-const Login = () => {
+const initialState = {
+  isSignedIn: false,
+  name: "",
+  email: "",
+  password: "",
+  photoURL: "",
+};
+
+const app = initializeApp(firebaseConfig);
+
+function Login() {
+  const [user, setUser] = useState(initialState);
+  const provider = new GoogleAuthProvider();
+
+  const handleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const { displayName, photoURL, email } = result.user;
+        console.log(displayName, photoURL, email);
+
+        const signedUser = {
+          isSignedIn: true,
+          name: displayName,
+          email,
+          photoURL,
+        };
+        setUser(signedUser);
+      })
+      .catch((error) => {
+        console.error(error);
+        const errorMessage = error.message;
+        console.error(errorMessage);
+      });
+  };
+
+  const handleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        setUser(initialState);
+      })
+      .catch((error) => {
+        console.error("Sign out error:", error);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    let isFormValid;
+
+    switch (name) {
+      case "email":
+        isFormValid = /\S+@\S+\.\S+/.test(value);
+        break;
+      case "password":
+        isFormValid = value.length > 6;
+        break;
+      case "name":
+        isFormValid = value.trim() !== "";
+        break;
+      default:
+        isFormValid = false;
+    }
+
+    if (isFormValid) {
+      setUser((prevUser) => ({
+        ...prevUser,
+        [name]: value,
+      }));
+    }
+  };
+
   return (
-    <div>
-      <h1>Login page</h1>
+    <div style={{textAlign: "center"}}>
+      {!user.isSignedIn ? (
+        <button onClick={handleSignIn}>Sign In</button>
+      ) : (
+        <button onClick={handleSignOut}>Sign Out</button>
+      )}
+
+      {user.isSignedIn && (
+        <div>
+          <p>Welcome, {user.name}</p>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="User profile" />
+        </div>
+      )}
+
+      <h1>Our Own Authentication System</h1>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <p>Password: {user.password}</p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+          onBlur={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onBlur={handleChange}
+          required
+        />
+        <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onBlur={handleChange}
+          required
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCuTyiZOhcbTmtPQJ4x2463DKf6HMn8Qo8",
+    authDomain: "ema-jhon-70db2.firebaseapp.com",
+    projectId: "ema-jhon-70db2",
+    storageBucket: "ema-jhon-70db2.appspot.com",
+    messagingSenderId: "510898768579",
+    appId: "1:510898768579:web:76b826139c815d261b3076",
+  };
+  
+  export default firebaseConfig;
+  
